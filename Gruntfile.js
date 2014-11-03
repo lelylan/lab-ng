6be4d39effbe86a9656019b443627dde8e7c9664
Andrea Reginato <andrea.reginato@gmail.com>
2014-11-03 15:49:34 +0100

    Fixed production config params

--- Gruntfile.js
+++ Gruntfile.js
@@ -387,7 +387,7 @@ module.exports = function (grunt) {
             credentials: {
               site: 'http://people.lelylan.com',
               clientId: 'c5297d9ad0710a3cfcd5983f369c8333c6cff60df1f2672cd5d3b2fed270c0d7',
-              redirectUri: 'http://lelylan.github.io/lab-dashboard-ng',
+              redirectUri: 'http://lelylan.github.io/lab-ng',
               profileUri: 'http://api.lelylan.com/me'
             }
           }
